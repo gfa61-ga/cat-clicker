880363ef708338b6e6a7430c2bbc0a7247e3b2d1
George Al <gfa61.ga@gmail.com>
2018-04-10 20:51:59 +0300

    feat: Add second requirements change

--- js/app.js
+++ js/app.js
@@ -16,33 +16,65 @@
             </div>
         `;
         this.addtoGame();
+        this.hide();
     };
 
     let gameContainer = document.querySelector('.game-container');
+    let catList = document.querySelector('.cat-list');
 
     Cat.prototype.addtoGame = function() {
-        let catContainer = document.createElement('div');
-        catContainer.innerHTML = this.html;
-        gameContainer.appendChild(catContainer);
+        this.catContainer = document.createElement('div');
+        this.catContainer.innerHTML = this.html;
+        gameContainer.appendChild(this.catContainer);
 
-        catContainer.addEventListener('click',
+        this.listItem = document.createElement('li');
+        this.listItem.innerHTML = `
+            <li>
+                <a href="#">${this.name}</a>
+            </li>
+        `;
+        catList.appendChild(this.listItem);
+
+        this.listItem.addEventListener('click', function(e) {
+                    currentCat.hide();
+                    let selectedCatName = e.target.innerText;
+                    currentCat = cats.find(cat => cat.name === selectedCatName);
+
+                    currentCat.show();
+            },
+            false
+        );
+
+        this.catContainer.addEventListener('click',
             (function() {
                 let clicks = 0;
                 return function(e) {
-                    if (e.target.classList.contains('cat-img')) {
-                        let clicksSpan = e.target.parentElement.querySelector('.clicks-span');
-                        clicks++;
-                        clicksSpan.innerText = clicks;
-                    }
+                    let clicksSpan = currentCat.catContainer.querySelector('.clicks-span');
+
+                    clicks++;
+                    clicksSpan.innerText = clicks;
                 };
             })(),
             false
         );
     };
 
+    Cat.prototype.hide = function() {
+        this.catContainer.style.display = "none";
+    };
+
+    Cat.prototype.show = function() {
+        this.catContainer.style.display = "block";
+    };
+
     let cats = [];
 
-    cats.push(new Cat("Cat1", "img/cat1.jpg"));
-    cats.push(new Cat("Cat2", "img/cat2.jpg"));
+    cats.push(new Cat("Pixie", "img/cat1.jpg"));
+    cats.push(new Cat("Venus", "img/cat2.jpg"));
+    cats.push(new Cat("Banjo & Mars", "img/cat3.jpg"));
+    cats.push(new Cat("Ruby", "img/cat4.jpg"));
+    cats.push(new Cat("Taffy", "img/cat5.jpg"));
 
+    let currentCat = cats[0];
+    cats[0].show();
 })();

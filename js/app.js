54d5d701d4ad9d9935c14d89e3470e787e5c5f52
George Al <gfa61.ga@gmail.com>
2018-04-14 17:12:41 +0300

    fix: Use Ben Jaffe's inspirational guidance

--- js/app.js
+++ js/app.js
@@ -2,38 +2,38 @@
 
     let model = {
         cats: [{
-                name: 'Pixie',
-                imgUrl: 'img/cat1.jpg',
-                clicks: 0
-            }, {
-                name: 'Venus',
-                imgUrl: 'img/cat2.jpg',
-                clicks: 0
-            }, {
-                name: 'Banjo & Mars',
-                imgUrl: 'img/cat3.jpg',
-                clicks: 0
-            }, {
-                name: 'Ruby',
-                imgUrl: 'img/cat4.jpg',
-                clicks: 0
-            }, {
-                name: 'Taffy',
-                imgUrl: 'img/cat5.jpg',
-                clicks: 0
-            }
-        ],
+            name: 'Pixie',
+            imgUrl: 'img/cat1.jpg',
+            clicks: 0
+        }, {
+            name: 'Venus',
+            imgUrl: 'img/cat2.jpg',
+            clicks: 0
+        }, {
+            name: 'Banjo & Mars',
+            imgUrl: 'img/cat3.jpg',
+            clicks: 0
+        }, {
+            name: 'Ruby',
+            imgUrl: 'img/cat4.jpg',
+            clicks: 0
+        }, {
+            name: 'Taffy',
+            imgUrl: 'img/cat5.jpg',
+            clicks: 0
+        }],
 
         currentCat: {}
     };
 
     let octapus = {
         init: function() {
-            viewCatList.init();
+            catListView.init();
 
-            let cats = octapus.getCats();
-            octapus.setCurrentCat(cats[0]);
-            viewSelectedCat.init();
+            model.currentCat = model.cats[0]; /*added*/
+            // let cats = octapus.getCats();
+            // octapus.setCurrentCat(cats[0]);
+            selectedCatView.init();
         },
 
         getCats: function() {
@@ -46,13 +46,79 @@
 
         getCurrentCat: function() {
             return model.currentCat;
-        }
+        },
+
+        incrementCurrentCatClicks: function() {  /*added*/
+            model.currentCat.clicks++;  /*added*/
+            selectedCatView.render();  /*added*/
+        }  /*added*/
+
     };
 
-    let viewCatList = {
+    let catListView = {
+
+/*
         init: function() {
             let catList = document.querySelector('.cat-list');
 
+            let cats = octapus.getCats();
+            for (let i=0; i<cats.length; i++) {
+
+                let listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    <li>
+                        <a href="#">${cats[i].name}</a>
+                    </li>
+                `;
+                catList.appendChild(listItem);
+
+                let cat = cats[i];  // With 'let' cat variable's scope is the for block
+                                    // That's why each EventListener see its own value of cat
+                listItem.addEventListener('click', function(e) {
+                        octapus.setCurrentCat(cat);
+
+                        selectedCatView.render();
+                    }
+                );
+            }
+        }
+*/
+
+/*
+        init: function() {
+            let catList = document.querySelector('.cat-list');
+
+            let cats = octapus.getCats();
+            for (let i=0; i<cats.length; i++) {
+
+                let listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    <li>
+                        <a href="#">${cats[i].name}</a>
+                    </li>
+                `;
+                catList.appendChild(listItem);
+
+                var cat = cats[i];  // With 'var' cat variable's scope is the init function
+                                    // That's why all EventListeners see the last value of cat: cats[4]
+                                    // To Solve this problem we pass tha cat value to each listener
+                                    // and create an inner closure to keep that value
+                listItem.addEventListener('click', (function(cat) {
+                    return function(e) {
+                            octapus.setCurrentCat(cat);
+                            selectedCatView.render();
+                        };
+                    }
+                )(cat));
+            }
+        }
+*/
+
+        init: function() {
+            let catList = document.querySelector('.cat-list');
+
+            // forEach creates a closure for each EventListener
+            // That's why each EventListener see its own value of cat
             octapus.getCats().forEach(function(cat) {
                 let listItem = document.createElement('li');
                 listItem.innerHTML = `
@@ -64,48 +130,54 @@
 
                 listItem.addEventListener('click', function(e) {
                         octapus.setCurrentCat(cat);
-
-                        viewSelectedCat.render();
-                    },
-                    false
+                        selectedCatView.render();
+                    }
                 );
             });
         }
     };
 
-    let viewSelectedCat = {
+    let selectedCatView = {
         init: function() {
-            viewSelectedCat.render();
-        },
+            let catContainer = document.querySelector('.cat-container');
 
-        render: function() {
-            let gameContainer = document.querySelector('.game-container');
-            let selectedCat = octapus.getCurrentCat();
+            catContainer.innerHTML = `
+                <div class="cat-name">
+                </div>
 
-            gameContainer.innerHTML = `
-                <div class="cat-container">
-                    <div class="cat-name">
-                        ${selectedCat.name}
-                    </div>
+                <img class="cat-img" src="" alt="A cat">
 
-                    <img class="cat-img" src=${selectedCat.imgUrl}>
-                    <div class="clics-container">
-                        Number of clicks: <span class="clicks-span">${selectedCat.clicks}</span>
-                    </div>
+                <div class="clics-container">
+                    Number of clicks: <span class="clicks-span"></span>
                 </div>
             `;
 
-            let catContainer = document.querySelector('.cat-container');
+            this.catName = document.querySelector('.cat-name');
+            this.catImg = document.querySelector('.cat-img');
+            this.catClicks = document.querySelector('.clicks-span');
 
-            catContainer.addEventListener('click',
+            this.catImg.addEventListener('click',
                 function() {
-                    let clicksSpan = catContainer.querySelector('.clicks-span');
-                    clicksSpan.innerText = ++selectedCat.clicks;
+                    // let clicksSpan = catContainer.querySelector('.clicks-span');
+                    // clicksSpan.innerText = ++selectedCat.clicks;
 
-                    octapus.setCurrentCat(selectedCat);
-                },
-                false
+                    octapus.incrementCurrentCatClicks(); /*added*/
+                    selectedCatView.render(); /*added*/
+                    //octapus.setCurrentCat(selectedCat);
+                }
             );
+
+            selectedCatView.render();
+        },
+
+        render: function() {
+            let selectedCat = octapus.getCurrentCat();
+
+            this.catName.innerText = selectedCat.name; // innerText returns the visible text contained in a node,
+                                                       // while textContent returns the full text
+            this.catImg.setAttribute('src', selectedCat.imgUrl);
+            // OR: this.catImg.src = selectedCat.imgUrl;
+            this.catClicks.innerText = selectedCat.clicks;
         }
     };
 
